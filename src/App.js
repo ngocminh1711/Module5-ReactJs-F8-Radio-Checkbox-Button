35e4b77e9056d72217a8ca00ee36beb1890cab9c
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-10-10 18:14:17 +0700

    add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
+const courses = [
+    {
+        id: 1,
+        name: 'HTML, CSS'
+    },
+    {
+        id: 2,
+        name: 'JavaScript'
+    },
+    {
+        id: 3,
+        name: 'ReactJS'
+    }
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [checkedRadio, setCheckedRadio] = useState(1)
+    const [checkedBox, setCheckedBox] = useState([])
+
+    const handleSubmit = () => {
+     console.log({ids: checkedBox})
+    }
+    const handleCheck = (id) => {
+           setCheckedBox(prev => {
+               const isChecked = checkedBox.includes(id)
+               if (isChecked){
+                  return checkedBox.filter(item => item !== id)
+               } else {
+                   return [...prev, id]
+               }
+           }
+
+    )
+    }
+
+    return (
+        <div className="App">
+            {courses.map(course => (
+                <div key={course.id}>
+                    <input
+                        type="radio"
+                        checked={checkedRadio === course.id}
+                        onChange={()=> handleCheck(course.id) }/>
+                    {course.name}
+                </div>
+            ))
+            }
+            {courses.map(course => (
+                <div key={course.id}>
+                    <input
+                        type="checkbox"
+                        checked={checkedBox.includes(course.id)}
+                        onChange={() =>
+                            handleCheck(course.id)
+                        }/>
+                    {course.name}
+                </div>
+            ))
+            }
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
